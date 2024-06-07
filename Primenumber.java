44016daae7b31d9d31c32bb8beb98a42503ad397
Sravani8747 <167780092+Sravani8747@users.noreply.github.com>
2024-06-07 10:58:10 -0700

    Update Primenumber.java

--- Primenumber.java
+++ Primenumber.java
@@ -1,19 +1,14 @@
-Class Super{    
- public static void main(String args[]){    
-  int i,m=0,flag=0;      
-  int n=3;//it is the number to be checked    
-  m=n/2;      
-  if(n==0||n==1){  
-   System.out.println(n+" is not prime number");      
-  }else{  
-   for(i=2;i<=m;i++){      
-    if(n%i==0){      
-     System.out.println(n+" is not prime number");      
-     flag=1;      
-     break;      
-    }      
-   }      
-   if(flag==0)  { System.out.println(n+" is prime number"); }  
-  }//end of else  
-}    
-}   
+class Total {
+  public static void main(String[] args) {
+    int x, y, sum;
+    Scanner myObj = new Scanner(System.in); // Create a Scanner object
+    System.out.println("Type a number:");
+    x = myObj.nextInt(); // Read user input
+
+    System.out.println("Type another number:");
+    y = myObj.nextInt(); // Read user input
+
+    sum = x + y;  // Calculate the sum of x + y
+    System.out.println("Sum is: " + sum); // Print the sum
+  }
+} 
